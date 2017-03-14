c4820ee96315a51f2f8d8efc6f9e7d85bedf41d6
SimonDaniel Altamirano <messi10gol@live.com>
2017-03-13 17:20:45 -0700

    Updated movement again!
    blah

--- 2DBoardGame/Assets/Movement.cs
+++ 2DBoardGame/Assets/Movement.cs
@@ -6,29 +6,45 @@ class Movement : MonoBehaviour
     Vector3 pos;                                // For movement
     float speed = 5.0f;                         // Speed of movement
 
+
     void Start()
     {
+		
         pos = transform.position;          // Take the initial position
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A) && transform.position == pos)
-        {        // Left
+		RaycastHit2D hitUp = Physics2D.Raycast(this.gameObject.transform.position, -Vector2.up, 1.0f);
+		RaycastHit2D hitDown = Physics2D.Raycast(this.gameObject.transform.position, -Vector2.down, 1.0f);
+		RaycastHit2D hitLeft = Physics2D.Raycast(this.gameObject.transform.position, -Vector2.left, 1.0f);
+		RaycastHit2D hitRight = Physics2D.Raycast(this.gameObject.transform.position, -Vector2.right, 1.0f);
+
+		if (Input.GetKey(KeyCode.A) && transform.position == pos)
+        { // Left
             pos += Vector3.left;
+			transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
         }
-        if (Input.GetKey(KeyCode.D) && transform.position == pos)
-        {        // Right
+		if (Input.GetKey(KeyCode.D) && transform.position == pos)
+        { // Right
             pos += Vector3.right;
+			transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
         }
-        if (Input.GetKey(KeyCode.W) && transform.position == pos)
-        {        // Up
-            pos += Vector3.up;
+		if (Input.GetKey(KeyCode.W) && transform.position == pos)
+		{ // Up
+            pos += Vector3.up; 
+			transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
         }
-        if (Input.GetKey(KeyCode.S) && transform.position == pos)
-        {        // Down
-            pos += Vector3.down;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
+		if (Input.GetKey (KeyCode.S) && transform.position == pos) 
+		{ // Down
+			pos += Vector3.down; 
+			transform.position = Vector2.MoveTowards (transform.position, pos, Time.deltaTime * speed);
+		} 
+		else {
+			Debug.Log (hitLeft.collider.name); 
+			Debug.Log (hitRight.collider.name); 
+			Debug.Log (hitUp.collider.name); 
+			Debug.Log (hitDown.collider.name); 
+		}
     }
 }
---
+++ 2DBoardGame/Assets/Movement1.cs
@@ -0,0 +1,63 @@
+﻿/* using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+	float speed = 5.0f;
+
+	[SerializeField]
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		RaycastHit2D hitUp = Physics2D.Raycast(transform.position, -Vector2.up);
+		RaycastHit2D hitDown = Physics2D.Raycast(transform.position, -Vector2.down);
+		RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, -Vector2.left);
+		RaycastHit2D hitRight = Physics2D.Raycast(transform.position, -Vector2.right);
+
+		if(collider !=null) 
+		{
+			if (Input.GetKey(KeyCode.A) && hitLeft.distance > 1)
+			{
+				transform.position += Vector3.left * speed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.D) && hitRight.distance > 1)
+			{
+				transform.position += Vector3.right * speed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.W) && hitUp.distance > 1)
+			{
+				transform.position += Vector3.up * speed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.S) && hitDown.distance > 1)
+			{
+				transform.position += Vector3.down * speed * Time.deltaTime;
+			}
+		}
+		else
+		{
+			if (Input.GetKey(KeyCode.J) && hitLeft.distance > 1)
+			{
+				transform.position += Vector3.left * speed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.L) && hitRight.distance > 1)
+			{
+				transform.position += Vector3.right * speed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.I) && hitUp.distance > 1)
+			{
+				transform.position += Vector3.up * speed * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.K) && hitDown.distance > 1)
+			{
+				transform.position += Vector3.down * speed * Time.deltaTime;
+			}
+		}
+	}
+}
+*/
